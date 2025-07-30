ec91cc04e61d7fe5fbd1edc444ca544456b0a72b
BRINDA <94870442+BrindaPragashkumar@users.noreply.github.com>
2025-07-30 21:21:38 +0100

    Time: 0 ms (100.00%) | Memory: 44.5 MB (50.26%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
@@ -1,21 +1,3 @@
-// class Solution {
-//     // https://mkyong.com/java/java-string-compareto-examples/
-//     public char nextGreatestLetter(char[] letters, char target) {
-
-//        for(int i=0; i<letters.length;i++){
-//             if ((letters[i].compareTo(target)) == 0){
-//                 return target;
-//             } else if ((letters[i].compareTo(target)) < 1){
-//                 return letters[i];
-//             }
-            
-//     }
-//      return letters[0];
-
-   
-// }}
-
-
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
         for (int i = 0; i < letters.length; i++) {
