a5bb1556bd5b56fb2e476777ca7547f7dfd7e34f
BRINDA <94870442+BrindaPragashkumar@users.noreply.github.com>
2025-10-04 16:30:34 +0100

    Time: 2 ms (98.92%) | Memory: 45.4 MB (5.15%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -11,9 +11,7 @@ class Solution {
             }
             map.put(nums[i],i);
         
-          
         }
-        return new int []{};
-        
+        return new int []{};        
     }
 }
