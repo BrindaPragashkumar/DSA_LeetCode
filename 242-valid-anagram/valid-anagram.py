4318ac6fc368ec7b3441852f1e7b858c2841f092
BRINDA <94870442+BrindaPragashkumar@users.noreply.github.com>
2025-10-18 14:50:16 +0100

    Time: 15 ms (85.74%) | Memory: 12.5 MB (82.36%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,25 +1,35 @@
 class Solution(object):
     def isAnagram(self, s, t):
+        # check lengths are the same
         if len(s) != len(t):
             return False
+
+        # create hash tables!
         f1 = {}
         f2 = {}
+
+        # count s chars
         for ch in s:
             if ch in f1:
                 f1[ch] += 1
             else:
                 f1[ch] = 1
+
+        # count t chars
         for ch in t:
             if ch in f2:
                 f2[ch] += 1
             else:
                 f2[ch] = 1
+
+        # compare both for differences
         for key in f1:
-            if key not in f2 or f1[key]!=f2[key]:
+            if key not in f2 or f1[key] != f2[key]:
                 return False
-        
+
+        # all matched successfully
         return True
-        
+
 
 
 
