ef36ed517ddb91510e94f1ccf6a6b3c0241ea203
BRINDA <94870442+BrindaPragashkumar@users.noreply.github.com>
2025-10-18 15:08:26 +0100

    Time: 11 ms (94.70%) | Memory: 12.6 MB (68.03%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,10 +1,27 @@
-from collections import Counter
-
 class Solution(object):
     def isAnagram(self, s, t):
         if len(s) != len(t):
             return False
-        return Counter(s) == Counter(t)
+        f1 = {}
+        f2 = {}
+
+        for ch in s:
+            if ch in f1:
+                f1[ch] += 1
+            else:
+                f1[ch] = 1
+        for ch in t:
+             if ch in f2:
+                f2[ch] += 1
+             else:
+                f2[ch] = 1
+        
+        for key in f1:
+            if key not in f2 or f1[key] != f2[key]:
+                return False
+
+        return True
+
         """
         :type s: str
         :type t: str
